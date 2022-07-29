61d172bc525249d857e85000083af90154c9c6f6
Munkh-Altai Chuluunbaatar <munkh-altai@MacBook-Pro.local>
2022-07-29 17:04:51 +0800

    back-end framework changed

--- public/assets/lambda/js/chunks/report-index.ea3bf72bff4e041d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[4735],{2146:(e,t,l)=>{l.r(t),l.d(t,{default:()=>u});const u=(0,l(51900).Z)({},(function(){var e=this.$createElement;return(this._self._c||e)("router-view")}),[],!1,null,null,null).exports}}]);
-//# sourceMappingURL=report-index.ea3bf72bff4e041d.js.map
