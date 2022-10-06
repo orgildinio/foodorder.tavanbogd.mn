4d9f94a1b56b67c1bc93575f18d9a359ad3f25af
Munkh-Altai Chuluunbaatar <munkh-altai@Munkh-Altais-MacBook-Pro-2.local>
2022-10-06 12:52:22 +0800

    ready

--- go.mod
+++ go.mod
@@ -3,18 +3,15 @@ module lambda
 go 1.15
 
 require (
-	github.com/99designs/gqlgen v0.17.5
-	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/gofiber/fiber/v2 v2.37.0-rc.1
 	github.com/gofiber/helmet/v2 v2.2.14
-	github.com/gorilla/websocket v1.5.0
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/lambda-platform/lambda v0.5.32
 	github.com/lib/pq v1.10.4 // indirect
-	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/onsi/gomega v1.20.0 // indirect
 	github.com/thedevsaddam/govalidator v1.9.10
-	github.com/valyala/fasthttp v1.39.0
-	github.com/vektah/gqlparser/v2 v2.4.2
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.23.6
 )
 
 //replace github.com/lambda-platform/lambda/puzzle v0.2.2 => ../../go/puzzle
---
+++ lambda/models/grid/Friends2.go
@@ -0,0 +1,81 @@
+package grid
+
+import "github.com/lambda-platform/lambda/datagrid"
+
+import "github.com/lambda-platform/lambda/models"
+
+import (
+	"github.com/lambda-platform/lambda/DB"
+	"gorm.io/gorm"
+	"time"
+)
+
+var _ = time.Time{}
+var _ = DB.Date{}
+var _ = gorm.DB{}
+
+type Friends2 struct {
+	ID    int    `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	Name  string `gorm:"column:name" json:"name"`
+	Phone string `gorm:"column:phone" json:"phone"`
+	Zurag string `gorm:"column:zurag" json:"zurag"`
+}
+
+type FriendsMainTable2 struct {
+	ID    int    `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	Name  string `gorm:"column:name" json:"name"`
+	Phone string `gorm:"column:phone" json:"phone"`
+	Zurag string `gorm:"column:zurag" json:"zurag"`
+}
+
+func (f *FriendsMainTable2) TableName() string {
+	return "friends"
+}
+
+//  TableName sets the insert table name for this struct type
+func (f *Friends2) TableName() string {
+	return "friends"
+}
+
+var Friends2Datagrid datagrid.Datagrid = datagrid.Datagrid{
+	Name:      "friends",
+	Identity:  "id",
+	DataTable: "friends",
+	MainTable: "friends",
+	DataModel: new(Friends2),
+	Data:      new([]Friends2),
+	MainModel: new(FriendsMainTable2),
+	Columns: []datagrid.Column{
+		datagrid.Column{Model: "name", Label: ""},
+		datagrid.Column{Model: "phone", Label: ""},
+		datagrid.Column{Model: "zurag", Label: ""},
+	},
+	ColumnList: []string{"id", "name", "phone", "zurag"},
+	Filters: map[string]string{
+		"id": "",
+
+		"name": "",
+
+		"phone": "",
+
+		"zurag": "",
+	},
+	Relations:   []models.GridRelation{},
+	Condition:   "",
+	Aggergation: "",
+	BeforeFetch: nil,
+
+	AfterFetch: nil,
+
+	BeforeDelete: nil,
+
+	AfterDelete: nil,
+
+	BeforePrint:        nil,
+	TriggerNameSpace:   "",
+	FillVirtualColumns: fillVirtualColumnsFriends2,
+}
+
+func fillVirtualColumnsFriends2(rowsPre interface{}) interface{} {
+	return rowsPre
+}
