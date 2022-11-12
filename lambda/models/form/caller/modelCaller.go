31e0904783cb714acc84f469337e61b69d79bf9b
Баялаг <40917206+madr88@users.noreply.github.com>
2022-11-12 13:06:05 +0800

    public custom regex addedd

--- lambda/models/form/caller/modelCaller.go
+++ lambda/models/form/caller/modelCaller.go
@@ -25,6 +25,81 @@ func GetMODEL(schema_id string) dataform.Dataform {
 	case "user_password":
 		return form.UsersDataform()
 
+	case "1":
+		return form.LutBarimtType1Dataform()
+
+	case "4":
+		return form.LutPositionType4Dataform()
+
+	case "6":
+		return form.LutPaymentType6Dataform()
+
+	case "66":
+		return form.FoodOrder66Dataform()
+
+	case "84":
+		return form.FoodOrder84Dataform()
+
+	case "86":
+		return form.FoodOrder86Dataform()
+
+	case "82":
+		return form.FoodOrder82Dataform()
+
+	case "8":
+		return form.LutOrderType8Dataform()
+
+	case "10":
+		return form.LutStaffType10Dataform()
+
+	case "14":
+		return form.TblCorp14Dataform()
+
+	case "83":
+		return form.FoodOrder83Dataform()
+
+	case "85":
+		return form.FoodOrder85Dataform()
+
+	case "98":
+		return form.LutMaterialUnit98Dataform()
+
+	case "68":
+		return form.FoodOrder68Dataform()
+
+	case "17":
+		return form.Users17Dataform()
+
+	case "100":
+		return form.LutMaterialStatus100Dataform()
+
+	case "18":
+		return form.TblFood18Dataform()
+
+	case "70":
+		return form.AddLocation70Dataform()
+
+	case "21":
+		return form.TblMenu21Dataform()
+
+	case "33":
+		return form.LutFoodType33Dataform()
+
+	case "35":
+		return form.TblOrderRule35Dataform()
+
+	case "53":
+		return form.TblMaterial53Dataform()
+
+	case "56":
+		return form.LutFoodTimeType56Dataform()
+
+	case "63":
+		return form.LutKitchenType63Dataform()
+
+	case "60":
+		return form.FoodOrder60Dataform()
+
 	}
 	return dataform.Dataform{}
 
--- lambda/models/grid/caller/modelCaller.go
+++ lambda/models/grid/caller/modelCaller.go
@@ -22,6 +22,78 @@ func GetMODEL(schema_id string) datagrid.Datagrid {
 	case "notification_target_grid":
 		return grid.NotificationTargetDatagrid
 
+	case "2":
+		return grid.LutBarimtType2Datagrid
+
+	case "5":
+		return grid.LutPositionType5Datagrid
+
+	case "9":
+		return grid.LutOrderType9Datagrid
+
+	case "7":
+		return grid.LutPaymentType7Datagrid
+
+	case "34":
+		return grid.LutFoodType34Datagrid
+
+	case "11":
+		return grid.LutStaffType11Datagrid
+
+	case "69":
+		return grid.ViewFoodOrder69Datagrid
+
+	case "16":
+		return grid.ViewCompany16Datagrid
+
+	case "20":
+		return grid.ViewFood20Datagrid
+
+	case "67":
+		return grid.ViewFoodOrder67Datagrid
+
+	case "71":
+		return grid.AddLocation71Datagrid
+
+	case "62":
+		return grid.ViewFoodOrder62Datagrid
+
+	case "101":
+		return grid.LutMaterialStatus101Datagrid
+
+	case "95":
+		return grid.MenuShoniinHool95Datagrid
+
+	case "57":
+		return grid.LutFoodTimeType57Datagrid
+
+	case "59":
+		return grid.ViewOrderRule59Datagrid
+
+	case "99":
+		return grid.LutMaterialUnit99Datagrid
+
+	case "92":
+		return grid.MenuOglooniiTsai92Datagrid
+
+	case "93":
+		return grid.MenuOdriinHool93Datagrid
+
+	case "64":
+		return grid.LutKitchenType64Datagrid
+
+	case "94":
+		return grid.MenuOroinHool94Datagrid
+
+	case "24":
+		return grid.ViewFoodMenu24Datagrid
+
+	case "96":
+		return grid.MenuZahialgatHool96Datagrid
+
+	case "55":
+		return grid.ViewMaterial55Datagrid
+
 	}
 	return datagrid.Datagrid{}
 
