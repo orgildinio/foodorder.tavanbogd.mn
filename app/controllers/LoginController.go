477492c37e17d614ce22dd230a3fbf5b8d9b21f7
Баялаг <40917206+madr88@users.noreply.github.com>
2023-01-05 10:22:24 +0800

    ERP Login done by

---
+++ app/controllers/LoginController.go
@@ -0,0 +1,477 @@
+package controllers
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/lambda-platform/lambda/DB"
+	"github.com/lambda-platform/lambda/agent/models"
+	agentUtils "github.com/lambda-platform/lambda/agent/utils"
+	"github.com/lambda-platform/lambda/config"
+	"io/ioutil"
+	appModel "lambda/app/models"
+	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type LoginData struct {
+	Token  string      `json:"token"`
+	Path   string      `json:"path"`
+	Status bool        `json:"status"`
+	Data   interface{} `json:"data"`
+}
+type User struct {
+	Login    string `json:"login" xml:"login" form:"login" query:"login"`
+	Password string `json:"password" xml:"password" form:"password" query:"password"`
+}
+type UserData struct {
+	Id    int64
+	Login string
+	Role  int64
+}
+type UserUUIDData struct {
+	Id    string
+	Login string
+	Role  int64
+}
+type jwtClaims struct {
+	Id    int64  `json:"id"`
+	Login string `json:"login"`
+	Role  int64  `json:"role"`
+	jwt.StandardClaims
+}
+type jwtUUIDClaims struct {
+	Id    string `json:"id"`
+	Login string `json:"login"`
+	Role  int64  `json:"role"`
+	jwt.StandardClaims
+}
+type Permissions struct {
+	DefaultMenu string      `json:"default_menu"`
+	Extra       interface{} `json:"extra"`
+	MenuID      int         `json:"menu_id"`
+	Permissions interface{} `json:"permissions"`
+}
+type LoginUser struct {
+	Login    string `json:"login" xml:"login" form:"login" query:"login"`
+	Password string `json:"password" xml:"password" form:"password" query:"password"`
+}
+type Unauthorized struct {
+	Error  string `json:"error"`
+	Status bool   `json:"status"`
+}
+
+//type LoginRes struct {
+//	Token string `json:"token"`
+//}
+
+type LoginRes struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+}
+
+func Login(c *fiber.Ctx) error {
+	fmt.Println("ERP LOGIN ========")
+	u := new(User)
+	if err := c.BodyParser(u); err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(models.Unauthorized{
+			Error:  "Username & password required",
+			Status: false,
+		})
+	}
+
+	foundUser := agentUtils.AuthUserObjectByLogin(u.Login)
+
+	if len(foundUser) == 0 {
+		if len(foundUser) == 0 {
+
+			return UbERPMobileLogin(u.Login, u.Password, c)
+
+		}
+
+	}
+
+	//password, err := Hash(u.Password)
+	//password_check1 := IsSame(password, foundUser.Password)
+	if agentUtils.IsSame(u.Password, foundUser["password"].(string)) {
+
+		var roleID int64 = 0
+		var userID int64 = 0
+		var userUUID string = ""
+
+		if reflect.TypeOf(foundUser["id"]).String() == "string" {
+			if config.Config.SysAdmin.UUID {
+				userUUID = foundUser["id"].(string)
+			} else {
+				i, err := strconv.ParseInt(foundUser["id"].(string), 10, 64)
+				if err != nil {
+					panic(err)
+				}
+				userID = i
+			}
+
+		} else {
+			userID = foundUser["id"].(int64)
+		}
+		if reflect.TypeOf(foundUser["role"]).String() == "string" {
+			i, err := strconv.ParseInt(foundUser["role"].(string), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			roleID = i
+		} else {
+			roleID = foundUser["role"].(int64)
+		}
+
+		if config.Config.SysAdmin.UUID {
+			// create jwt token
+			token, err := createUUIDJwtToken(UserUUIDData{Id: userUUID, Login: foundUser["login"].(string), Role: roleID})
+			if err != nil {
+				//log.Println("Error Creating JWT token", err)
+				return c.Status(fiber.StatusUnauthorized).JSON(models.Unauthorized{
+					Error:  "Unauthorized",
+					Status: false,
+				})
+			}
+
+			cookie := new(fiber.Cookie)
+			cookie.Name = "token"
+			cookie.Path = "/"
+			cookie.Value = token
+			cookie.Expires = time.Now().Add(time.Hour * time.Duration(config.Config.JWT.Ttl))
+			//cookie.HttpOnly = true
+			//cookie.Secure = true
+
+			delete(foundUser, "password")
+
+			foundUser["jwt"] = token
+
+			c.Cookie(cookie)
+
+			return c.Status(fiber.StatusOK).JSON(models.LoginData{
+				Token:  token,
+				Path:   checkRole(roleID),
+				Status: true,
+				Data:   foundUser,
+			})
+		} else {
+			// create jwt token
+			token, err := createJwtToken(UserData{Id: userID, Login: foundUser["login"].(string), Role: roleID})
+			if err != nil {
+				//log.Println("Error Creating JWT token", err)
+				return c.Status(fiber.StatusUnauthorized).JSON(models.Unauthorized{
+					Error:  "Unauthorized",
+					Status: false,
+				})
+			}
+
+			cookie := new(fiber.Cookie)
+			cookie.Name = "token"
+			cookie.Path = "/"
+			cookie.Value = token
+			cookie.Expires = time.Now().Add(time.Hour * time.Duration(config.Config.JWT.Ttl))
+
+			delete(foundUser, "password")
+
+			foundUser["jwt"] = token
+
+			c.Cookie(cookie)
+
+			return c.Status(fiber.StatusOK).JSON(models.LoginData{
+				Token:  token,
+				Path:   checkRole(roleID),
+				Status: true,
+				Data:   foundUser,
+			})
+		}
+
+	}
+
+	return c.Status(fiber.StatusUnauthorized).JSON(models.Unauthorized{
+		Error:  "Unauthorized",
+		Status: false,
+	})
+
+}
+func createUUIDJwtToken(user UserUUIDData) (string, error) {
+	// Set custom claims
+	claims := jwt.MapClaims{
+		"id":    user.Id,
+		"login": user.Login,
+		"role":  user.Role,
+		"exp":   time.Now().Add(time.Hour * time.Duration(config.Config.JWT.Ttl)).Unix(),
+	}
+	// Create token with claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString([]byte(config.Config.JWT.Secret))
+	if err != nil {
+		return "", err
+	}
+	return t, nil
+}
+func checkRole(role int64) string {
+	for _, r := range config.LambdaConfig.RoleRedirects {
+		if role == r.RoleID {
+			return r.URL
+		}
+	}
+
+	RoleData := map[string]interface{}{}
+
+	jsonFile, err := os.Open("lambda/role_" + fmt.Sprintf("%v", role) + ".json")
+
+	defer jsonFile.Close()
+	if err != nil {
+		foundRole := models.Role{}
+		DB.DB.Where("id = ?", role).First(&foundRole)
+		if foundRole.Permissions != "" {
+
+			Permissions := models.Permissions{}
+			json.Unmarshal([]byte(foundRole.Permissions), &Permissions)
+			if Permissions.DefaultMenu != "" {
+				return config.LambdaConfig.AppURL + Permissions.DefaultMenu
+			}
+		}
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &RoleData)
+
+	permissonData, err := json.Marshal(RoleData["permissions"])
+	if err != nil {
+		return "/auth/login"
+	}
+
+	permissions := models.Permissions{}
+	json.Unmarshal(permissonData, &permissions)
+	return config.LambdaConfig.AppURL + permissions.DefaultMenu
+
+}
+
+func createJwtToken(user UserData) (string, error) {
+	// Set custom claims
+	claims := jwt.MapClaims{
+		"id":    user.Id,
+		"login": user.Login,
+		"role":  user.Role,
+		"exp":   time.Now().Add(time.Hour * time.Duration(config.Config.JWT.Ttl)).Unix(),
+	}
+	// Create token with claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString([]byte(config.Config.JWT.Secret))
+	if err != nil {
+		return "", err
+	}
+	return t, nil
+}
+
+func UbERPMobileLogin(Loging string, Password string, c *fiber.Ctx) error {
+	fmt.Println("ERP")
+	url := "http://202.21.112.3:8082/api/auth-service/login"
+	method := "POST"
+
+	str := Loging
+	splited := strings.Split(str, `\`)
+
+	fmt.Println(Loging)
+	fmt.Println(Password)
+	fmt.Println(splited)
+
+	if len(splited) >= 2 {
+
+		companyID := strings.ToUpper(splited[0])
+		userID := splited[1]
+
+		//fmt.Println(Loging)
+		//fmt.Println(Password)
+		fmt.Println("{\"password\": \"" + Password + "\",  \"userid\": \"" + userID + "\", \"companyid\": \"" + companyID + "\"}")
+
+		payload := strings.NewReader("{\"password\": \"" + Password + "\",  \"userid\": \"" + userID + "\", \"companyid\": \"" + companyID + "\"}")
+
+		client := &http.Client{}
+		req, err := http.NewRequest(method, url, payload)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		req.Header.Add("Content-Type", "application/json-patch+json")
+		//req.Header.Add("Content-Type", "text/plain")
+
+		res, err := client.Do(req)
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+
+		loginRES := LoginRes{}
+		errParse := json.Unmarshal([]byte(body), &loginRES)
+		if errParse != nil || loginRES.AccessToken == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(models.Unauthorized{
+				Error:  "User not found",
+				Status: false,
+			})
+		} else {
+			return checkUserByToken(loginRES.AccessToken, c)
+		}
+
+	} else {
+		return c.Status(fiber.StatusUnauthorized).JSON(models.Unauthorized{
+			Error:  "User not found",
+			Status: false,
+		})
+	}
+}
+
+type JWTPayLoad struct {
+	UserID     string `json:"userID"`
+	EmpID      string `json:"EmpID"`
+	CompanyID  string `json:"CompanyID"`
+	DepID      string `json:"DepID"`
+	PositionID string `json:"positionID"`
+	Ipaddress  string `json:"ipaddress"`
+	Macaddress string `json:"macaddress"`
+	Sub        string `json:"sub"`
+	Jti        string `json:"jti"`
+	Iat        string `json:"iat"`
+	Nbf        int    `json:"nbf"`
+	Exp        int    `json:"exp"`
+	Iss        string `json:"iss"`
+	Aud        string `json:"aud"`
+}
+
+func checkUserByToken(token string, c *fiber.Ctx) error {
+	splited := strings.Split(token, `.`)
+	uDec, _ := base64.RawStdEncoding.DecodeString(splited[1])
+
+	payload := JWTPayLoad{}
+	if err := json.Unmarshal(uDec, &payload); err != nil {
+		fmt.Println(err.Error())
+	}
+	//fmt.Println(splited[1])
+	//fmt.Println(string(uDec))
+	fmt.Println(payload.EmpID)
+
+	url := "http://202.21.112.3:8082/api/hrms/hr/employee/get?EmployeeID=" + payload.EmpID
+	fmt.Println(url)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("Content-Type", "application/json; charset=utf-8")
+
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+	userInfo := appModel.UserInfo{}
+	err := json.Unmarshal([]byte(body), &userInfo)
+	if err != nil {
+		fmt.Println(err.Error())
+		return c.Status(fiber.StatusUnauthorized).JSON(models.Unauthorized{
+			Error:  "User not found",
+			Status: false,
+		})
+
+	} else {
+
+		fmt.Println(len(userInfo.Retdata))
+		fmt.Println(len(userInfo.Retdata))
+		fmt.Println(len(userInfo.Retdata))
+		if len(userInfo.Retdata) >= 1 {
+
+			return erpKhorooByToken(c, userInfo, true)
+
+		} else {
+			return c.Status(fiber.StatusUnauthorized).JSON(models.Unauthorized{
+				Error:  "User not found",
+				Status: false,
+			})
+		}
+
+	}
+	return c.Status(fiber.StatusUnauthorized).JSON(models.Unauthorized{
+		Error:  "User not found",
+		Status: false,
+	})
+
+}
+
+func erpKhorooByToken(c *fiber.Ctx, userInfo appModel.UserInfo, isKhoroo bool) error {
+
+	foundUser := agentUtils.AuthUserObjectByLogin(userInfo.Retdata[0].Regno)
+	fmt.Println(len(foundUser))
+	if len(foundUser) == 0 {
+
+		// role changed
+		var role int64 = 3
+
+		// empID added
+		newUser := appModel.UserERP{
+			Login:          userInfo.Retdata[0].Regno,
+			Email:          userInfo.Retdata[0].Email,
+			FirstName:      userInfo.Retdata[0].Firstname,
+			LastName:       userInfo.Retdata[0].Firstname,
+			Phone:          userInfo.Retdata[0].Mobilephone,
+			RegisterNumber: userInfo.Retdata[0].Regno,
+			Role:           role,
+			Birthday:       time.Now(),
+			Status:         "1",
+			Gender:         "m",
+			EmpID:          int(userInfo.Retdata[0].Empid),
+		}
+
+		DB.DB.Create(&newUser)
+
+		foundUser2 := agentUtils.AuthUserObjectByLogin(newUser.Login)
+
+		token, err := createJwtToken(UserData{Id: newUser.ID, Login: newUser.Login, Role: newUser.Role})
+		if err != nil {
+			return c.JSON(Unauthorized{
+				Error:  "Username & password required",
+				Status: false,
+			})
+		}
+
+		if isKhoroo {
+			delete(foundUser2, "password")
+
+			foundUser2["jwt"] = token
+			return c.JSON(LoginData{
+				Token:  token,
+				Path:   "/control#/p/956471dd-88fb-2803-d1ab-302dd63c30c1/0e15ca07-13b5-f9de-aade-4bddd30066fd",
+				Status: true,
+				Data:   foundUser2,
+			})
+		}
+
+	} else {
+		// create jwt token
+		token, err := createJwtToken(UserData{Id: foundUser["id"].(int64), Login: foundUser["login"].(string), Role: foundUser["role"].(int64)})
+		if err != nil {
+			return c.JSON(Unauthorized{
+				Error:  "Username & password required",
+				Status: false,
+			})
+		}
+
+		if isKhoroo {
+			delete(foundUser, "password")
+
+			foundUser["jwt"] = token
+			return c.JSON(LoginData{
+				Token:  token,
+				Path:   "",
+				Status: true,
+				Data:   foundUser,
+			})
+		}
+	}
+
+	return c.JSON(Unauthorized{
+		Error:  "Username & password required",
+		Status: false,
+	})
+
+}
---
+++ app/models/login.go
@@ -0,0 +1,171 @@
+package models
+
+import "time"
+
+type UserInfo struct {
+	Affectedrows int `json:"affectedrows"`
+	Retdata      []struct {
+		Empid                 float32     `json:"empid"`
+		Code                  string      `json:"code"`
+		Familyname            interface{} `json:"familyname"`
+		Familynameenus        interface{} `json:"familynameenus"`
+		Lastname              string      `json:"lastname"`
+		Lastnameenus          string      `json:"lastnameenus"`
+		Firstname             string      `json:"firstname"`
+		Firstnameenus         string      `json:"firstnameenus"`
+		Gender                string      `json:"gender"`
+		Gendername            interface{} `json:"gendername"`
+		Gendernameenus        interface{} `json:"gendernameenus"`
+		Birthdate             string      `json:"birthdate"`
+		Birthcountryid        interface{} `json:"birthcountryid"`
+		Birthcountrycode      interface{} `json:"birthcountrycode"`
+		Birthcountryname      interface{} `json:"birthcountryname"`
+		BirthcountrynameEnus  interface{} `json:"birthcountryname_enus"`
+		Birthdivisionid       interface{} `json:"birthdivisionid"`
+		Birthdivisioncode     interface{} `json:"birthdivisioncode"`
+		Birthdivisionname     interface{} `json:"birthdivisionname"`
+		BirthdivisionnameEnus interface{} `json:"birthdivisionname_enus"`
+		Birthdistrictid       interface{} `json:"birthdistrictid"`
+		Birthdistrictcode     interface{} `json:"birthdistrictcode"`
+		Birthdistrictname     interface{} `json:"birthdistrictname"`
+		BirthdistrictnameEnus interface{} `json:"birthdistrictname_enus"`
+		Birthplace            interface{} `json:"birthplace"`
+		Countryid             interface{} `json:"countryid"`
+		Countrycode           interface{} `json:"countrycode"`
+		Countryname           interface{} `json:"countryname"`
+		CountrynameEnus       interface{} `json:"countryname_enus"`
+		Divisionid            interface{} `json:"divisionid"`
+		Divisioncode          interface{} `json:"divisioncode"`
+		Divisionname          interface{} `json:"divisionname"`
+		DivisionnameEnus      interface{} `json:"divisionname_enus"`
+		Districtid            interface{} `json:"districtid"`
+		Districtcode          interface{} `json:"districtcode"`
+		Districtname          interface{} `json:"districtname"`
+		DistrictnameEnus      interface{} `json:"districtname_enus"`
+		Nationalityid         interface{} `json:"nationalityid"`
+		Nationalitycode       interface{} `json:"nationalitycode"`
+		Nationalityname       interface{} `json:"nationalityname"`
+		NationalitynameEnus   interface{} `json:"nationalityname_enus"`
+		Socialoriginid        interface{} `json:"socialoriginid"`
+		Socialorigincode      interface{} `json:"socialorigincode"`
+		Socialoriginname      interface{} `json:"socialoriginname"`
+		SocialoriginnameEnus  interface{} `json:"socialoriginname_enus"`
+		Maritalstatus         string      `json:"maritalstatus"`
+		Maritalstatusname     interface{} `json:"maritalstatusname"`
+		MaritalstatusnameEnus interface{} `json:"maritalstatusname_enus"`
+		Bloodgroup            string      `json:"bloodgroup"`
+		Bloodgroupname        interface{} `json:"bloodgroupname"`
+		BloodgroupnameEnus    interface{} `json:"bloodgroupname_enus"`
+		Regno                 string      `json:"regno"`
+		Passno                interface{} `json:"passno"`
+		Forpassno             interface{} `json:"forpassno"`
+		Emdno                 interface{} `json:"emdno"`
+		Nddno                 interface{} `json:"nddno"`
+		Addr1                 interface{} `json:"addr1"`
+		Addr2                 interface{} `json:"addr2"`
+		Homephone             interface{} `json:"homephone"`
+		Mobilephone           string      `json:"mobilephone"`
+		Workphone             interface{} `json:"workphone"`
+		Workphoneext          interface{} `json:"workphoneext"`
+		Fax                   interface{} `json:"fax"`
+		Email                 string      `json:"email"`
+		Postaddress           interface{} `json:"postaddress"`
+		Contactname           interface{} `json:"contactname"`
+		Contactphone          interface{} `json:"contactphone"`
+		Withpicture           string      `json:"withpicture"`
+		Status                string      `json:"status"`
+		Statusname            interface{} `json:"statusname"`
+		StatusnameEnus        interface{} `json:"statusname_enus"`
+		Companyid             string      `json:"companyid"`
+		Companyname           string      `json:"companyname"`
+		Statusdate            string      `json:"statusdate"`
+
+		Email2          interface{} `json:"email2"`
+		Contactname2    interface{} `json:"contactname2"`
+		Contactphone2   interface{} `json:"contactphone2"`
+		Isblacklisted   interface{} `json:"isblacklisted"`
+		Blacklistreason interface{} `json:"blacklistreason"`
+		Blacklisteddate interface{} `json:"blacklisteddate"`
+
+		Militaryserved  string      `json:"militaryserved"`
+		Trainingsport   interface{} `json:"trainingsport"`
+		Interest        interface{} `json:"interest"`
+		Appid           interface{} `json:"appid"`
+		Militaryno      interface{} `json:"militaryno"`
+		Ecompanyid      string      `json:"ecompanyid"`
+		Emptype         string      `json:"emptype"`
+		Emptypename     interface{} `json:"emptypename"`
+		EmptypenameEnus interface{} `json:"emptypename_enus"`
+		Depid           string      `json:"depid"`
+		Depcode         string      `json:"depcode"`
+		Depname         string      `json:"depname"`
+		DepnameEnus     interface{} `json:"depname_enus"`
+
+		Poscode       string      `json:"poscode"`
+		Posname       string      `json:"posname"`
+		PosnameEnus   interface{} `json:"posname_enus"`
+		Gradeid       int         `json:"gradeid"`
+		Gradecode     string      `json:"gradecode"`
+		Gradename     string      `json:"gradename"`
+		GradenameEnus interface{} `json:"gradename_enus"`
+		Levelcode     string      `json:"levelcode"`
+		Levelname     string      `json:"levelname"`
+		LevelnameEnus string      `json:"levelname_enus"`
+		Hireddate     string      `json:"hireddate"`
+
+		Basewagecurrcode string `json:"basewagecurrcode"`
+
+		Joineddate         interface{} `json:"joineddate"`
+		Terminateddate     interface{} `json:"terminateddate"`
+		Terminatedreasonid interface{} `json:"terminatedreasonid"`
+		Contractid         interface{} `json:"contractid"`
+
+		Picturedata       interface{} `json:"picturedata"`
+		Picturetumb       interface{} `json:"picturetumb"`
+		Signaturedata     interface{} `json:"signaturedata"`
+		Countryworkedyear string      `json:"countryworkedyear"`
+		Groupworkedyear   string      `json:"groupworkedyear"`
+		Companyworkedyear string      `json:"companyworkedyear"`
+		Reason            interface{} `json:"reason"`
+	} `json:"retdata"`
+	Rettype       int         `json:"rettype"`
+	Depfilter     interface{} `json:"depfilter"`
+	Retparams     interface{} `json:"retparams"`
+	Retmsg        string      `json:"retmsg"`
+	Retstacktrace string      `json:"retstacktrace"`
+	Traceno       int         `json:"traceno"`
+	Totalrow      int         `json:"totalrow"`
+	Totalpage     int         `json:"totalpage"`
+}
+
+type UserERP struct {
+	ID             int64      `gorm:"column:id;primary_key" json:"id"`
+	CreatedAt      *time.Time `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt      *time.Time `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt      *time.Time `gorm:"column:deleted_at" json:"deleted_at"`
+	Status         string     `gorm:"column:status;type:ENUM('0','1','2')" json:"status"`
+	Role           int64      `gorm:"column:role" json:"role"`
+	Login          string     `gorm:"column:login;unique_index;not null;unique" json:"login"`
+	Email          string     `gorm:"column:email;unique_index;not null;unique" json:"email"`
+	RegisterNumber string     `gorm:"column:register_number;not null;unique" json:"register_number"`
+	Avatar         string     `gorm:"column:avatar;type:TEXT" json:"avatar"`
+	Bio            string     `gorm:"column:bio;type:TEXT" json:"bio"`
+	FirstName      string     `gorm:"column:first_name" json:"first_name"`
+	LastName       string     `gorm:"column:last_name" json:"last_name"`
+	Birthday       time.Time  `gorm:"column:birthday;type:DATE" json:"birthday"`
+	Phone          string     `gorm:"column:phone" json:"phone"`
+	//DistrictID     int        `gorm:"column:district_id" json:"district_id"`
+	//RegionId       int        `gorm:"column:region_id" json:"region_id"`
+	//OrgId          string     `gorm:"column:org_id" json:"org_id"`
+	//UnitId         string     `gorm:"column:unit_id" json:"unit_id"`
+	//PositionId     string     `gorm:"column:position_id" json:"position_id"`
+	Gender   string `gorm:"column:gender;type:ENUM('f','m')" json:"gender"`
+	Password string `gorm:"column:password" json:"password"`
+	FcmToken string `gorm:"column:fcm_token" json:"fcm_token"`
+	EmpID    int    `gorm:"column:emp_id" json:"emp_id"`
+}
+
+// TableName sets the insert table name for this struct type
+func (v *UserERP) TableName() string {
+	return "users"
+}
