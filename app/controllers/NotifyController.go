00ad3cbea2b561131e30eb4b6aec362520873393
munkh-altai <munkaltai@gmail.com>
2023-05-15 10:07:08 +0800

    notify started

--- app/controllers/NotifyController.go
+++ app/controllers/NotifyController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/lambda-platform/lambda/DB"
 	"github.com/lambda-platform/lambda/config"
@@ -44,4 +45,37 @@ func LeftOverQuantity(c *fiber.Ctx) error {
 	}
 	notifyHandler.CreateNotification(data, map[string]interface{}{})
 	return c.JSON(data)
+}
+
+func LeftOverQuantitySend(FoodID int, KitchenID int, Quantity int) {
+
+	food := models.TblFood{}
+
+	DB.DB.Where("food_id = ? AND kitchen_id = ?", FoodID, KitchenID).Find(&food)
+
+	FCMData := modelsModels.FCMData{
+		Title:       "Хоолны үлдэгдэл дуусж байна",
+		Body:        fmt.Sprintf("%s гал тогооны, %s хоолны үлдэгдэл дуусж байна. Үлдэгдэл %d !!!", "1-р", food.FoodName, Quantity),
+		FirstName:   "Систем",
+		Sound:       config.LambdaConfig.Notify.Sound,
+		Icon:        config.LambdaConfig.Favicon,
+		Link:        "/admin",
+		ClickAction: config.LambdaConfig.Domain + "/admin",
+	}
+
+	FCMNotification := modelsModels.FCMNotification{
+		Title:       "Хоолны үлдэгдэл дуусж байна",
+		Body:        fmt.Sprintf("%s гал тогооны, %s хоолны үлдэгдэл дуусж байна. Үлдэгдэл %d !!!", "1-р", food.FoodName, Quantity),
+		Icon:        config.LambdaConfig.Domain + "/" + config.LambdaConfig.Favicon,
+		ClickAction: config.LambdaConfig.Domain + "/admin",
+	}
+
+	data := modelsModels.NotificationData{
+		//Users: []int{17},
+		Roles:        []int{3},
+		Data:         FCMData,
+		Notification: FCMNotification,
+	}
+	notifyHandler.CreateNotification(data, map[string]interface{}{})
+
 }
